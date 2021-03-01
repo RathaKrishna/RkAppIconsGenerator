99efc71c52a606d92816b45daadff250569227ce
Rathakrishnan <born4winkrishna@gmail.com>
2021-03-01 10:02:22 +0800

    Initial Commit

---
+++ RkAppIconsGenerator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RkAppIconsGenerator
+//
+//  Created by Rathakrishnan on 01/03/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
